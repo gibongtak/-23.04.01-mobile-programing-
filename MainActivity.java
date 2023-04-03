9f17c8605061549f57f48b6cdecd2f7c48bd1aed
gibongtak <128627739+gibongtak@users.noreply.github.com>
2023-04-03 16:38:37 +0900

    [23.04.01] 모바일  프로그래밍 과제(four button event)
    activity_main.xml: 버튼 4개 추가.
    strings.xml: 버튼 4개 string 변수 추가.
    MainActivity.java: 각 4개의 버튼의 색깔 변경 코드 작성. 각 4개의 버튼 시 이벤트 발생하는 추가 코드 작성.

--- MainActivity.java
+++ MainActivity.java
@@ -1,26 +1,64 @@
-package com.example.ex230331;
+package com.example.ex230401;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button)findViewById(R.id.button1);//xml의 Button과 java를 연결해주는 명령어..
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button1.setBackgroundColor(Color.GRAY);
+        button2.setBackgroundColor(Color.GREEN);
+        button3.setBackgroundColor(Color.RED);
+        button4.setBackgroundColor(Color.YELLOW);
+
+        button1.setTextColor(Color.BLACK);
+        button2.setTextColor(Color.BLACK);
+        button3.setTextColor(Color.BLACK);
+        button4.setTextColor(Color.BLACK);
+
+        button1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                //Toast = 화면 하단에 뜨는 팝업 알림의 명칭, LENGTH_SHORT는 알림이 떠있는 시간을 짧게 한다는 의미.
-                Toast.makeText(getApplicationContext(),"버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            public void onClick(View view){
+                Intent mIntent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mIntent1);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mIntent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mIntent2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mIntent3 = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent3);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
             }
         });
     }
